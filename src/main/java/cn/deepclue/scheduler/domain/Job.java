d9cb3245982493b36f56dfcf4b8d65ef81b45c9a
sunxingwen <sunxingwen@deepclue.cn>
2017-07-17 14:38:43 +0800

    modify job add serialize and deserialize

--- src/main/java/cn/deepclue/scheduler/domain/Job.java
+++ src/main/java/cn/deepclue/scheduler/domain/Job.java
@@ -1,9 +1,20 @@
 package cn.deepclue.scheduler.domain;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by xuzb on 17/03/2017.
  */
 public class Job {
+    private static final String JID_KEY = "JID";
+    private static final String APPID_KEY = "APPID";
+    private static final String CALLBACK_KEY = "CALLBACK";
+
     private int jId;
     private int appId;
     private Callback callback;
@@ -41,5 +52,32 @@ public class Job {
 
     public void setCallback(Callback callback) {
         this.callback = callback;
+    }
+
+    public boolean run() {
+        return true;
+    }
+
+    public String serialize() {
+        Map<String, String> dataMap = new HashMap<>();
+        dataMap.put(JID_KEY, String.valueOf(jId));
+        dataMap.put(APPID_KEY, String.valueOf(appId));
+        dataMap.put(CALLBACK_KEY, new Gson().toJson(callback));
+        return new Gson().toJson(dataMap);
+    }
+
+    public void deserialize(String jsonMap) {
+        Type mapType = new TypeToken<Map<String, String>>() {
+        }.getType();
+        Map<String, String> dataMap = new Gson().fromJson(jsonMap, mapType);
+
+        String jidKey = dataMap.get(JID_KEY);
+        this.jId = Integer.valueOf(jidKey);
+
+        String appidKey = dataMap.get(APPID_KEY);
+        this.appId = Integer.valueOf(appidKey);
+
+        String callbackKey = dataMap.get(CALLBACK_KEY);
+        this.callback = new Gson().fromJson(callbackKey, Callback.class);
     }
 }
